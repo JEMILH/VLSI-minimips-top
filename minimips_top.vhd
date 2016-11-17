7e42167e92aeb05e4d3ba140ac7a4c416eebbafd
Mathieu Léonardon <mathieu.leonardon@gmail.com>
2016-11-17 16:27:56 -0500

    Add WB and MEWB implementation

--- minimips_top.vhd
+++ minimips_top.vhd
@@ -16,63 +16,68 @@ use ieee.numeric_std.all;
 use std.textio.all;
 
 entity minimips_top is
-    port(
-	in_rst_n : in std_logic;
-	in_clk : in std_logic;
-	in_imem_read: in std_logic_vector(31 downto 0);
-	in_dmem_read: in std_logic_vector(31 downto 0);
-	out_dmem_write: out std_logic_vector(31 downto 0);
-	out_dmem_we: out std_logic;
-	out_dmem_addr: out std_logic_vector(9 downto 0);
-	out_imem_addr: out std_logic_vector(9 downto 0));
+  port(
+    in_rst_n       : in  std_logic;
+    in_clk         : in  std_logic;
+    in_imem_read   : in  std_logic_vector(31 downto 0);
+    in_dmem_read   : in  std_logic_vector(31 downto 0);
+    out_dmem_write : out std_logic_vector(31 downto 0);
+    out_dmem_we    : out std_logic;
+    out_dmem_addr  : out std_logic_vector(9 downto 0);
+    out_imem_addr  : out std_logic_vector(9 downto 0));
 end entity minimips_top;
 
 architecture beh of minimips_top is
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<FETCH>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal IF_PC : std_logic_vector(31 downto 0);
+  signal IF_PC : std_logic_vector(31 downto 0);
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<IFID>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
 
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DECODE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal ID_rs
-	signal ID_rt
-	signal ID_opcode
-	signal ID_funct
-	signal ID_im
-	signal ID_target
-	signal ID_RS
-	signal ID_RT
-	signal ID_wb_addr
-	signal ID_control
-	signal ID_Sim
-	signal ID_ta
+  signal ID_rs
+    signal ID_rt
+    signal ID_opcode
+    signal ID_funct
+    signal ID_im
+    signal ID_target
+    signal ID_RS
+    signal ID_RT
+    signal ID_wb_addr
+    signal ID_control
+    signal ID_Sim
+    signal ID_ta
+    signal ID_lw : std_logic;
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<IDEX>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal IDEX_RS
-	signal IDEX_RT
-	signal IDEX_wb_addr
-	signal IDEX_control
-	signal IDEX_Sim
-	signal IDEX_ta
+  signal IDEX_RS
+    signal IDEX_RT
+    signal IDEX_wb_addr
+    signal IDEX_control
+    signal IDEX_Sim
+    signal IDEX_ta
+    signal IDEX_lw : std_logic;
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<EXECUTE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal EX_branch
-	signal EX_ta
-	signal EX_alu
-	signal EX_dmem_addr
-	signal EX_dmem_write
-	
+  signal EX_branch
+    signal EX_ta
+    signal EX_alu
+    signal EX_dmem_addr
+    signal EX_dmem_write
+
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<EXME>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal EXME_alu
-	signal EXME_dmem_addr
-	signal EXME_dmem_write
-	signal EXME_wb_addr
+    signal EXME_alu
+    signal EXME_dmem_addr
+    signal EXME_dmem_write
+    signal EXME_wb_addr
+    signal EXME_lw : std_logic;
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MEMORY>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-  
+
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MEWB>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal MEWB_alu
-	signal MEWB_wb_addr
-	
+  signal MEWB_alu
+      signal MEWB_dmem
+    signal MEWB_wb_addr
+    signal MEWB_lw : std_logic;
+
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<WRITEBACK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-	signal WB_data
-begin   
+  signal WB_data
+begin
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<FETCH>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
 
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<IFID>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
@@ -80,32 +85,28 @@ begin
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<DECODE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
 
 
-
-
-
-
-
-
-
-
-
-
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<IDEX>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
 
-
-
-
-
-
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<EXECUTE>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-  
+
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<EXME>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-  
+
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MEMORY>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-  
+
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MEWB>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-  
+  MEWB : process (in_clk, in_rst_n) is
+  begin  -- process MEWB
+    if in_rst_n = '0' then              -- asynchronous reset (active low)
+      MEWB_alu     <= (others => '0');
+      MEWB_wb_addr <= (others => '0');
+      MEWB_lw      <= '0';
+    elsif in_clk'event and in_clk = '1' then  -- rising clock edge
+      MEWB_alu     <= EXME_alu;
+      MEWB_wb_addr <= EXME_wb_addr;
+      MEWB_lw      <= EXME_lw;
+    end if;
+  end process MEWB;
 --<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<WRITEBACK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--
-  
+  WB_data <= MEWB_dmem when MEWB_lw = '1' else MEWB_alu;
   
 end architecture beh;
